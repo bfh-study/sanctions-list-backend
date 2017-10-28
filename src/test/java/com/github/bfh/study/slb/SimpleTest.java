68d6ad6b949604e6402de98a4e512bae01e93e19
Samuel Ackermann <saemi.ackermann@gmail.com>
2017-10-28 12:11:35 +0200

    add unit test library and write an example test

---
+++ src/test/java/com/github/bfh/study/slb/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.github.bfh.study.slb;
+
+import org.testng.annotations.Test;
+
+/**
+ * Example test
+ */
+@Test
+public class SimpleTest {
+
+    public void testBoolean() {
+        assert Boolean.TRUE.equals(true);
+    }
+}
