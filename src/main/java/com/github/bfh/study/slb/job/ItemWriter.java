4487f465a60d07b5ec15ec044be4455af83a6e9b
Samuel Ackermann <saemi.ackermann@gmail.com>
2017-11-07 19:16:54 +0100

    change indentation level

--- src/main/java/com/github/bfh/study/slb/job/ItemWriter.java
+++ src/main/java/com/github/bfh/study/slb/job/ItemWriter.java
@@ -9,23 +9,23 @@ import java.util.List;
  * @author Samuel Ackermann <saemi.ackermann@gmail.com>
  */
 public class ItemWriter extends ReaderWriterBase implements javax.batch.api.chunk.ItemWriter {
-  @Override
-  public void open(Serializable checkpoint) throws Exception {
-    this.checkpoint = checkpoint;
-  }
+    @Override
+    public void open(Serializable checkpoint) throws Exception {
+        this.checkpoint = checkpoint;
+    }
 
-  @Override
-  public void close() throws Exception {
-    String test = "nothing to close! it is a dummy!";
-  }
+    @Override
+    public void close() throws Exception {
+        String test = "nothing to close! it is a dummy!";
+    }
 
-  @Override
-  public void writeItems(List<Object> items) throws Exception {
-    items.forEach(o -> o.toString());
-  }
+    @Override
+    public void writeItems(List<Object> items) throws Exception {
+        items.forEach(o -> o.toString());
+    }
 
-  @Override
-  public Serializable checkpointInfo() throws Exception {
-    return checkpoint;
-  }
+    @Override
+    public Serializable checkpointInfo() throws Exception {
+        return checkpoint;
+    }
 }
