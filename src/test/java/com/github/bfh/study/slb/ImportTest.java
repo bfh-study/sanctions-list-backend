215a140b2beb35357732b8c3441f2150bd62583f
Samuel Ackermann <saemi.ackermann@gmail.com>
2017-11-15 15:18:45 +0100

    add config step

--- src/test/java/com/github/bfh/study/slb/ImportTest.java
+++ src/test/java/com/github/bfh/study/slb/ImportTest.java
@@ -1,6 +1,7 @@
 package com.github.bfh.study.slb;
 
 import com.github.bfh.study.slb.imports.EuProvider;
+import com.github.bfh.study.slb.imports.ImportContext;
 import com.github.bfh.study.slb.imports.SecoProvider;
 import org.testng.annotations.Test;
 
@@ -12,12 +13,12 @@ public class ImportTest {
     ImportContext context;
 
     public void secoProviderTest(){
-        context = new ImportContext(new SecoProvider());
+        context = ImportContext.importContextFactory("SECO");
         assertEquals(15, context.executeImport(10, 5));
     }
 
     public void euProviderTest(){
-        context = new ImportContext(new EuProvider());
+        context = ImportContext.importContextFactory("EU");
         assertEquals(5, context.executeImport(10, 5));
     }
 }
