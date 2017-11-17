2800988dd92251c2e586ec9c2dab1d3af2e8be66
Samuel Ackermann <saemi.ackermann@gmail.com>
2017-11-17 10:27:39 +0100

    use transient user data for job context

--- src/main/java/com/github/bfh/study/slb/imports/job/XmlReader.java
+++ src/main/java/com/github/bfh/study/slb/imports/job/XmlReader.java
@@ -1,11 +1,11 @@
 package com.github.bfh.study.slb.imports.job;
 
-import com.github.bfh.study.slb.ImportContext;
+import com.github.bfh.study.slb.imports.ImportContext;
 import com.github.bfh.study.slb.imports.parser.PartialXmlParser;
-import com.github.bfh.study.slb.imports.SecoProvider;
-
 import java.io.Serializable;
 import javax.batch.api.chunk.ItemReader;
+import javax.batch.runtime.context.JobContext;
+import javax.inject.Inject;
 
 /**
  * parse a defined xml and get the elements back
@@ -16,22 +16,33 @@ public class XmlReader extends ReaderWriterBase implements ItemReader {
 
     private PartialXmlParser parser;
 
+    @Inject
+    private JobContext jobContext;
+
     @Override
     public void open(Serializable checkpoint) throws Exception {
         this.checkpoint = checkpoint;
-        ImportContext context = new ImportContext(new SecoProvider());
-        parser = new PartialXmlParser(context.getProcessingElements());
-        parser.open("");
+        ImportContext context = null;
+        try {
+            context = (ImportContext) jobContext.getTransientUserData();
+        } catch (ClassCastException e) {
+            jobContext.setExitStatus("No import context found.");
+            return;
+        }
+        //parser = new PartialXmlParser(context.getProcessingElements());
+        //parser.open("");
     }
 
     @Override
     public void close() throws Exception {
-        parser.close();
+        //parser.close();
     }
 
     @Override
     public Object readItem() throws Exception {
-        return parser.parse();
+        //Object o = parser.parse();
+        //checkpoint += (Serializable) 1;
+        return null ;
     }
 
     @Override
