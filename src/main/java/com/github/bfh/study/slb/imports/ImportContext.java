0aa207a03682342db7829513ed3cc261776a0936
Samuel Ackermann <saemi.ackermann@gmail.com>
2017-12-06 19:35:48 +0100

    add query param for controlling which file should be process and add it as property to the job

--- src/main/java/com/github/bfh/study/slb/imports/ImportContext.java
+++ src/main/java/com/github/bfh/study/slb/imports/ImportContext.java
@@ -26,6 +26,7 @@ public class ImportContext {
 
     public static ImportContext importContextFactory(String sourceName)
         throws SourceNotFoundException {
+        
         Import importer = null;
         switch (sourceName) {
             case "SECO":
--- src/main/java/com/github/bfh/study/slb/imports/job/PrepareStep.java
+++ src/main/java/com/github/bfh/study/slb/imports/job/PrepareStep.java
@@ -3,6 +3,9 @@ package com.github.bfh.study.slb.imports.job;
 import com.github.bfh.study.slb.imports.ImportContext;
 import com.github.bfh.study.slb.imports.SourceNotFoundException;
 
+import java.util.Properties;
+import javax.batch.runtime.BatchRuntime;
+import javax.batch.runtime.context.StepContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
@@ -28,12 +31,15 @@ public class PrepareStep implements Batchlet {
 
     @Override
     public String process() throws Exception {
-        String sourceName = jobContext.getProperties().getProperty(SOURCE_NAME_PROPERTY);
+        Properties properties = BatchRuntime.getJobOperator().getParameters(
+            jobContext.getExecutionId());
+        String sourceName = properties.getProperty(SOURCE_NAME_PROPERTY);
         Thread.sleep(2000L);
         try {
             jobContext.setTransientUserData(ImportContext.importContextFactory(sourceName));
         } catch (SourceNotFoundException e) {
             logger.warn(String.format("%s Job will stop", e.getMessage()));
+            BatchRuntime.getJobOperator().stop(jobContext.getExecutionId());
             return BatchStatus.FAILED.toString();
         }
 
