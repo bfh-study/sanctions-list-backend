b0624a6528a1e8a5ab1ccaa001561dffa7139fd2
Samuel Ackermann <saemi.ackermann@gmail.com>
2017-12-17 16:12:37 +0100

    implement entity processor and item writer

--- src/main/java/com/github/bfh/study/slb/imports/job/EntityProcessor.java
+++ src/main/java/com/github/bfh/study/slb/imports/job/EntityProcessor.java
@@ -1,11 +1,23 @@
 package com.github.bfh.study.slb.imports.job;
 
+import com.github.bfh.study.slb.domain.entities.SanctionEntity;
+import com.github.bfh.study.slb.domain.entities.SanctionProgram;
 import com.github.bfh.study.slb.imports.ImportContext;
 
+import org.bitbucket.samsamann.rest.base.entities.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import javax.batch.api.chunk.ItemProcessor;
 import javax.batch.runtime.context.JobContext;
 import javax.inject.Inject;
 
+/**
+ * convert items from the different sources into domain entities.
+ *
+ * @author Samuel Ackermann
+ */
 public class EntityProcessor implements ItemProcessor {
 
     @Inject
@@ -13,9 +25,12 @@ public class EntityProcessor implements ItemProcessor {
 
     private ImportContext context;
 
-    /*public EntityProcessor() {
+    private List<SanctionProgram> sanctionPrograms;
+
+    public EntityProcessor() {
         context = null;
-    }*/
+        sanctionPrograms = new ArrayList<>();
+    }
 
     @Override
     public Object processItem(Object item) throws Exception {
@@ -23,6 +38,21 @@ public class EntityProcessor implements ItemProcessor {
             context = (ImportContext) jobContext.getTransientUserData();
         }
 
-        return item;
+        BaseEntity baseEntity = context.convertEntity(item);
+        if (baseEntity instanceof SanctionProgram) {
+            sanctionPrograms.add((SanctionProgram) baseEntity);
+        } else if (baseEntity instanceof SanctionEntity) {
+            SanctionEntity entity = (SanctionEntity) baseEntity;
+            Optional<SanctionProgram> optional = sanctionPrograms.stream()
+                .filter(p -> p.getSanctionIdentifier().equals(entity.getSanctionIdentifier()))
+                .findFirst();
+
+            if (optional.isPresent()) {
+                entity.setSanctionProgram(optional.get());
+                return entity;
+            }
+        }
+
+        return baseEntity;
     }
 }
